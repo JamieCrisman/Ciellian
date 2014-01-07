bee252f7cec779a488dc0eb907cd035a542778a9
jamie crisman <jamie@awkwardhero.com>
2014-01-07 14:55:17 -0600

    some cleanup

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ Array.prototype.clean = function(deleteValue) {
 
 
 var restify = require("restify"),
-	userSave = require("save")('user'),
 	mongoose = require("mongoose");//,
-	//hash = require('./pass').hash;
 var fs = require('fs');
 eval(fs.readFileSync('pass.js')+''); //including the pass.js which includes the edit password. super hackish
 
@@ -67,10 +65,6 @@ server.use(restify.bodyParser());
 restify.defaultResponseHeaders =function(data){
 	this.contentType = 'json';
 }
-
-
-//seed
-//userSave.create({name: 'AwkwardHero'});
 
 //mongo setup
 mongoose.connect("mongodb://localhost/myapp");
@@ -492,105 +486,3 @@ server.del('/word/:word', function(req, res, next){
 	
 });
 
-
-/*
-server.post('/login', function(req,res,next){
-	if(req.params.username === undefined || req.params.username == "" && req.params.username == req.params.username.replace(/\W/g, '')){
-		return next(new restify.InvalidArgumentError('Invalid Username'));
-	}
-	authenticate(req.params.username.toLowerCase(), req.params.password, function(error, user){
-		if(error){
-			return next( new restify.InvalidArgumentError( "Invalid User Or Pass" ) );
-		}
-		res.send(201,{token: user.token, message: "Login successful"});
-	});
-});
-
-server.post('/user', function(req, res, next){
-	if(req.params.password != req.params.confirmPassword){
-		return next(new restify.InvalidArgumentError( "Passwords did not match" ) );
-	}
-	if(req.params.username === undefined || req.params.username == "" && req.params.username == req.params.username.replace(/\W/g, '')){
-		return next(new restify.InvalidArgumentError('Invalid Username'));
-	}
-	User.find({username: req.params.username.toLowerCase()}, function(err, user){
-		//console.log("users found____________");
-		//console.log(user);
-		if(user.length > 0){
-			//console.log("username taken");
-			return next(new restify.InvalidArgumentError('Username Taken'));
-		}else{
-			hash(req.params.password, function(err, salt, hash){
-				if(err) return next( new restify.InvalidArgumentError( "Error: "+JSON.stringify(err.errors) ) );
-				User.create({
-					username: req.params.username.toLowerCase(),
-					password: hash,
-						salt: salt,
-					   token: req.params.username.toLowerCase() + "-" + (hash+"").substring(Math.floor(hash.length/8), Math.ceil(hash.length/0.25))
-				}, function(error, user){
-					if (error){
-						return next( new restify.InvalidArgumentError( "Error: "+JSON.stringify(error.errors) ) );
-					};
-					//console.log(user);
-					res.send(201,{token: user.token, message: "User created"});
-				});
-			});
-		}
-	});
-});
-
-server.post('/testToken', function(req,res,next){
-	if(req.params.token === undefined || req.params.token == null){
-		res.send({isValid: false, message: "Please Insert 1 Token(s)"});
-	}
-
-	var who = req.params.token.split("-")[0];
-	//console.log(who);
-	//console.log(req.params.token);
-	User.findOne({username: who.toLowerCase(), token: req.params.token}, function(error, user){
-		//console.log(error);
-		if(user != null)
-			res.send({isValid: true});
-		else
-			res.send({isValid: false, message: "Invalid Token"});
-	});
-});
-
-server.del('/user/:username', function(req, res, next){
-	//this is totally safe</sarcasm>
-	if(req.params.secret == "BOSSKEY25"){
-		User.remove({username: req.params.username.toLowerCase()}, function(error, user){
-			if(error){
-				return next( new restify.InvalidArgumentError( JSON.stringify(error.errors) ) );
-			}
-			res.send();
-		});
-	}else{
-		res.send();
-	}
-	
-});
-
-
-//helper functions
-
-function authenticate(name, pass, fn){
-	if(!module.parent) console.log("Authenticating: %s:%s", name, pass);
-
-	User.findOne({username: name}, function(err, user){
-		if(user){
-			if(err){
-				return fn( new restify.InvalidArgumentError( JSON.stringify(err.errors) ) );
-			}
-			hash(pass, user.salt, function(err, hash){
-				if(err) return fn( new restify.InvalidArgumentError( JSON.stringify(err.errors) ) );
-				if(hash == user.password) return fn(null, user);
-				return fn( new restify.InvalidArgumentError( "Invalid User or Pass" ) );
-			});
-		}else{
-			return fn( new restify.InvalidArgumentError( "Invalid User or Pass" ) );
-		}
-	});
-}
-
-*/
